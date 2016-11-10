70b1b47bc7eaa6d45dda0acf4a1c902392e4daeb
tim <tpdnjs135@gmail.com>
2016-11-11 01:41:48 +0900

    시작 씬, 스테이지 불러오는 작업

--- PushPush/Assets/Scripts/GameManager.cs
+++ PushPush/Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PushPush.Manager
 {
     public class GameManager : MonoBehaviour
     {
+        #region singleton
         private static GameManager instance;
         public static GameManager Instance
         {
@@ -24,20 +26,28 @@ namespace PushPush.Manager
                 return instance;
             }
         }
+        #endregion
 
-        public Stage Stage;
+        public Dictionary<int, Stage> Stages = new Dictionary<int, Stage>();
         public bool GameClear = false;
+        public bool GameStart = false;
+        public int CurrentStageNumber = -1;
+
+        public Stage CurrentStage = null;
+
+
+        private GameObject field = null;
 
         public void UpdateSituation()
         {
-            if (Stage == null)
+            /*if (Stage == null)
                 Stage = GameObject.FindObjectOfType<Stage>();
 
             foreach (var home in Stage.Homes)
             {
                 if (home.haveBall == false)
                     return;
-            }
+            }*/
 
             GameClear = true;
 
@@ -45,6 +55,52 @@ namespace PushPush.Manager
             Debug.Log("Clear!");
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
+        }
+
+        public bool Touch()
+        {
+#if UNITY_EDITOR
+            return Input.GetMouseButtonDown(0);
+#else
+            return Input.touchCount > 0;
+#endif
+        }
+
+        public void StartGame()
+        {
+            CurrentStageNumber = PlayerPrefs.GetInt("CurrentStage", 0);
+            var stageTemp = Resources.LoadAll<Stage>("Prefabs/Stage/").ToList();
+
+            for(int i = 0; i < stageTemp.Count; ++i)
+                Stages.Add(i + 1, stageTemp[i]);
+
+            field = GameObject.Find("Field");
+
+            LoadStage();
+        }
+
+        public void LoadStage()
+        {
+            if (CurrentStageNumber == -1 || field == null || Stages == null || Stages.Count == 0)
+            {
+                EndGame();
+                return;
+            }
+
+            if (CurrentStage != null)
+            {
+                Destroy(CurrentStage);
+                CurrentStage = null;
+            }
+
+            CurrentStage = Instantiate<Stage>(Stages[CurrentStageNumber]);
+            CurrentStage.transform.SetParent(field.transform);
+            CurrentStage.transform.position = Vector3.zero;
+        }
+
+        public void EndGame()
+        {
+            PlayerPrefs.SetInt("CurrentStage", CurrentStageNumber);
         }
     }
 }
---
+++ PushPush/Assets/Scripts/Title.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using PushPush.Manager;
+
+public class Title : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        
+        if (GameManager.Instance.Touch() && GameManager.Instance.GameStart == false)
+        {
+            GameManager.Instance.StartGame();
+
+            gameObject.SetActive(false);
+        }
+	}
+}
